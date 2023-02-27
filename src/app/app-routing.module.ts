16ba5020e5f6ee482de2e4e6aadef034e023a049
Andresvilla27 <108768637+Andresvilla27@users.noreply.github.com>
2023-02-27 11:04:26 -0300

    separacion de repositorio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EditAcercaDeComponent } from './componentes/acerca-de/edit-acerca-de.component';
+import { EditeducacionComponent } from './componentes/estudios/editeducacion.component';
+import { NeweducacionComponent } from './componentes/estudios/neweducacion.component';
+import { EditExperienciaComponent } from './componentes/experiencia/edit-experiencia.component';
+import { NewExperienciaComponent } from './componentes/experiencia/new-experiencia.component';
+import { EditSkillComponent } from './componentes/habilidades/edit-skill.component';
+import { NewSkillComponent } from './componentes/habilidades/new-skill.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { LoginComponent } from './componentes/login/login.component';
+
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path: 'nuevaexp', component:NewExperienciaComponent},
+  {path: 'editexp/:id', component:EditExperienciaComponent},
+  {path: 'nuevaedu', component:NeweducacionComponent},
+  {path: 'editedu/:id', component:EditeducacionComponent},
+  {path: 'newskill', component: NewSkillComponent},
+  {path: 'editskill/:id', component: EditSkillComponent},
+  {path: 'editacercade/:id', component: EditAcercaDeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,29 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'portfolio-andres-4c470',
+    appId: '1:526198863298:web:13b37ad31c9619cce0525b',
+    storageBucket: 'portfolio-andres-4c470.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyD3NGMNBSKCq__UTUz_vJqeYp0XPH9nOIA',
+    authDomain: 'portfolio-andres-4c470.firebaseapp.com',
+    messagingSenderId: '526198863298',
+    measurementId: 'G-FWWG6BDSQ7',
+  },
+  production: false,
+
+  URL: 'https://backend-portfolio-kksa.onrender.com/'
+  
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
